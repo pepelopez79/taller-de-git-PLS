35d9123c17960f16248d7310c7e05f11323a2812
Pepe López <pepe.lopez.salado7@gmail.com>
2024-05-15 20:13:36 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php
 // Autor: Pepe López <pepelopez@gmail.com>
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
